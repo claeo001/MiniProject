514c5418a7042bce4d5b134fa2d4c0fe2cc581a8
YeongWoooo <rdd9223@gmail.com>
2021-03-25 10:44:45 +0900

    Fix: DeprecationWarning 문제 해결

--- app.js
+++ app.js
@@ -1,65 +1,63 @@
-const express = require('express');
-const path = require('path');
-const http = require('http');
-const webpack = require('webpack');
-const session = require('express-session');
-const redisStore = require('connect-redis')(session);
-const redis = require('redis');
-const bodyParser = require('body-parser');
-const mongoose = require('mongoose');
-const sessionSecretKey = require('./config/sessionSecretKey');
-const middlewares = require('./middlewares');
-const url = 'mongodb://localhost:27017/app';
+const express = require("express");
+const path = require("path");
+const http = require("http");
+const webpack = require("webpack");
+const session = require("express-session");
+const redisStore = require("connect-redis")(session);
+const redis = require("redis");
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
 
-const initExpress = redisClient => {
-	const app = express();
-	const PORT = 3000;
+const middlewares = require("./middlewares");
 
-	app.use(bodyParser.json());
-	app.use(bodyParser.urlencoded({ extended: true }));
-	app.use(
-		session({
-			key: 'app.sid',
-			// 세션 시크릿 키 따로 저장
-			secret: sessionSecretKey,
-			store: new redisStore({
-				host: '127.0.0.1',
-				port: 6379,
-				client: redisClient,
-				prefix: 'session:',
-				db: 0
-			}),
-			resave: false,
-			saveUninitialized: true,
-			cookie: { path: '/', maxAge: 1800000 }
-		})
-	);
-	app.use(express.static(path.resolve('./dist/client')));
-	app.use(express.static(path.resolve('./statics')));
-	app.use(express.static(path.resolve('./node_modules')));
-	app.use('/', require('./routes/view'));
-	app.use('/api', require('./routes/api'));
-	app.use(middlewares.error.logging);
-	app.use(middlewares.error.ajaxHandler);
-	app.use(middlewares.error.handler);
+const initExpress = (redisClient) => {
+  const app = express();
+  const PORT = 3000;
 
-	return require('http')
-		.createServer(app)
-		.listen(PORT, () => {
-			console.log('Express server listening on port ' + PORT);
-		});
+  app.use(bodyParser.json());
+  app.use(bodyParser.urlencoded({ extended: true }));
+  app.use(
+    session({
+      key: "app.sid",
+      secret: "session-secret",
+      store: new redisStore({
+        host: "127.0.0.1",
+        port: 6379,
+        client: redisClient,
+        prefix: "session:",
+        db: 0,
+      }),
+      resave: false,
+      saveUninitialized: true,
+      cookie: { path: "/", maxAge: 1800000 },
+    })
+  );
+  app.use(express.static(path.resolve("./dist/client")));
+  app.use(express.static(path.resolve("./statics")));
+  app.use(express.static(path.resolve("./node_modules")));
+  app.use("/", require("./routes/view"));
+  app.use("/api", require("./routes/api"));
+  app.use(middlewares.error.logging);
+  app.use(middlewares.error.ajaxHandler);
+  app.use(middlewares.error.handler);
+
+  return require("http")
+    .createServer(app)
+    .listen(PORT, () => {
+      console.log("Express server listening on port " + PORT);
+    });
 };
 
 const initRedis = () => redis.createClient();
 const initMongo = async () => {
-	await mongoose.connect(url, { useNewUrlParser: true });
+  await mongoose.connect("mongodb://localhost:27017/app", { useNewUrlParser: true, useUnifiedTopology: true });
 };
 
 const main = () => {
-	initMongo().then(() => {
-		const redisClient = initRedis();
-		const server = initExpress(redisClient);
-	});
+  initMongo().then(() => {
+    const redisClient = initRedis();
+    const server = initExpress(redisClient);
+  });
 };
 
 main();
