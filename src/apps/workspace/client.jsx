d7d16bada8a20c9caa725d774f992a9effc39537
YeongWoooo <rdd9223@gmail.com>
2021-03-26 15:00:26 +0900

    Style: 포멧팅 변경

--- src/apps/workspace/client.jsx
+++ src/apps/workspace/client.jsx
@@ -1,14 +1,14 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { BrowserRouter } from 'react-router-dom';
+import React from "react";
+import ReactDOM from "react-dom";
+import { BrowserRouter } from "react-router-dom";
 
-import App from './index';
+import App from "./index";
 
 const data = window.__INITIAL_STATE__;
 
 ReactDOM.hydrate(
-	<BrowserRouter>
-		<App {...data} />
-	</BrowserRouter>,
-	document.getElementById('app')
+  <BrowserRouter>
+    <App {...data} />
+  </BrowserRouter>,
+  document.getElementById("app")
 );
